567a24639521eb176c38cecb76b5e84ee690b36b
Felipe Frantz Zanini <devffzanini@gmail.com>
2020-11-09 19:53:30 -0300

    Validando dadtas
    Adicionado a extensão date-fns para trabalhar com as datas. Anteriormente foi adicionado o uuidv4 para aplicar um id global unico para os registros

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,18 +1,31 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [
+interface Appointment {
+  id: string,
+  provider: string,
+  date: Date
+}
 
-];
+const appointments: Appointment[] = [];
+
 appointmentsRouter.post('/', (request, response) => {
   const {provider, date } = request.body;
+
+  const parserdDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parserdDate, appointment.date))
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This appointment is already booked'});
+  }
 
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parserdDate,
   }
 
   appointments.push(appointment);
